062ab980a916db9536310fb5a3627b13803bea5d
Michael-Enciso-Yancce <michaelencisoyancce@gmail.com>
2025-11-22 17:54:29 -0500

    Michael

--- target/generated-sources/annotations/com/portfolio/michael/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/portfolio/michael/mapper/UserMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-22T17:25:15-0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
+    date = "2025-11-22T17:54:10-0500",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
@@ -21,10 +21,10 @@ public class UserMapperImpl implements UserMapper {
 
         User.UserBuilder user = User.builder();
 
-        user.name( userRequest.getName() );
-        user.lastName( userRequest.getLastName() );
-        user.password( userRequest.getPassword() );
         user.email( userRequest.getEmail() );
+        user.lastName( userRequest.getLastName() );
+        user.name( userRequest.getName() );
+        user.password( userRequest.getPassword() );
 
         return user.build();
     }
