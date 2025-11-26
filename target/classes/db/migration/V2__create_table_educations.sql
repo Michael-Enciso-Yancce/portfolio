4e7550d3f2abb1c26321657f56c8f28f2f922caa
Michael-Enciso-Yancce <michaelencisoyancce@gmail.com>
2025-11-25 22:39:34 -0500

    Remove target folder from tracking

--- target/classes/db/migration/V2__create_table_educations.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS educations (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    institution VARCHAR(255),
-    degree      VARCHAR(255),
-    start_date  DATE NOT NULL,
-    end_date    DATE NOT NULL
-);
